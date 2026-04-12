6b695812045c6a85a882bc7a162bbc2e6cda7b08
Jonas Binding <jonas@familiebinding.de>
2026-04-12 17:21:38 +0200

    Updated dependencies, removed gulpfile (and gulp dependency) for now. Attempt to show failure of JSON function with automated test (but test not fully correct yet)

--- test/hunspell-tojson.js
+++ test/hunspell-tojson.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
-const Spellchecker = require('../index');
+const Spellchecker = require('../lib/index');
 
 describe('hunspell-tojson', () => {
     const fixturesDir = path.join(__dirname, 'fixtures');
